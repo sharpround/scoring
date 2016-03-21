d0e9618c7488c7977e6b1e62783a65e1df8b7225
David Boy <david.a.boy@gmail.com>
2016-03-21 17:01:36 -0400

    feat: Add all category urls

--- kaf_spider.py
+++ kaf_spider.py
@@ -4,31 +4,44 @@ import scrapy
 
 class KingArthurFlourSpider(scrapy.Spider):
     name = 'kingarthurflour'
-    start_urls = ['http://www.kingarthurflour.com/recipes/bread/no-knead?page=all']
+    start_urls = ['http://www.kingarthurflour.com/recipes/bread/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/breakfast-and-brunch/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/cake/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/cookies-bars-and-candy/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/entres-sides-and-appetizers/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/favorite-classics/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/frozen-treats/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/gluten-free/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/miscellaneous/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/pies-tarts-and-turnovers/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/pizza-and-flatbread/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/scones/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/sourdough/view-all?page=all',
+                  'http://www.kingarthurflour.com/recipes/whole-wheat/whole-grain-/view-all?page=all']
+
 
     def parse(self, response):
-        links = response.xpath('a[id="rec_URL_1A"]/@href') + \
-        		response.xpath('a[id="rec_URL_2A"]/@href')
+        links = response.xpath('//a[@id="rec_URL_1A"]/@href') + response.xpath('//a[@id="rec_URL_2A"]/@href')
 
         for href in links:
             full_url = response.urljoin(href.extract())
             yield scrapy.Request(full_url, callback=self.parse_recipe)
 
-    def parse_category(self, response):
-        pass
-
     def parse_recipe(self, response):
         yield {
+            'url':              response.url,
             'name':             response.xpath('//*[@itemprop="name"]/text()').extract(),
             'rating':           response.xpath('//*[@itemprop="ratingValue"]/@content').extract(),
             'num_ratings':      response.xpath('//*[@itemprop="reviewCount"]/text()').extract(),
             'date':             response.xpath('//*[@itemprop="datePublished"]/@content').extract(),
-            'kaf_guaranteed':   response.xpath('//*[@data-serverid="kafGuaranteed_flag"]').extract(),
+            'kaf_guaranteed':   response.xpath('//*[@data-serverid="kafGuaranteed_flag"]/text()').extract(),
             'headnotes':        response.xpath('//*[@data-serverid="HeadNotes"]/text()').extract(),
-            'ingredients':      response.xpath('//*[@id="g_ingredients"]//li').extract(),
+            'v_ingredients':    response.xpath('//*[@id="v_ingredients"]//li').extract(),
+            'w_ingredients':    response.xpath('//*[@id="w_ingredients"]//li').extract(),
+            'g_ingredients':    response.xpath('//*[@id="g_ingredients"]//li').extract(),
             'instructions':     response.xpath('//*[@itemprop="recipeInstructions"]/li/span/text()').extract()
+        }
             # 'author':           response.xpath('//*[@data-serverid="recipe_author"]').extract(),
             # 'prep_time':        response.xpath('//*[@itemprop="prepTime"]').extract(),
             # 'cook_time':        response.xpath('//*[@itemprop="prepTime"]').extract(),
             # 'prep_time':        response.xpath('//*[@itemprop="prepTime"]').extract(),
-            }
